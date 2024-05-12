e17575588c54dbbaa120845324c4f0750603dda2
Divyesh1692 <divyeshvariya1692@gmail.com>
2024-05-12 14:33:59 +0530

    Largest Of 3

--- Assignment/Find Max of 3/max.js
+++ Assignment/Find Max of 3/max.js
@@ -4,23 +4,15 @@ function Largest()
     let b = parseInt(document.getElementById("n2").value);
     let c = parseInt(document.getElementById("n3").value);
     let max;
-    if(a>b)
+    if(a>b && a>c)
         {
-            if(a>c)
-                {
-                    max = a;
-                }
-            else{ max = c;}
+             max = a;
         }
-    else
+    else if (b>a && b> c)
         {
-            if(b>c)
-                {
-                    max = b;
-                }
-            else { max =c; }
+            max = b;
         }
-    
+    else max = c;
     document.getElementById("max").innerHTML = "Largest Of Three Numbers is " + max;
 
 }
