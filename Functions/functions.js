b81f7cf2a639440a904e097aff4e66a9580aa1a3
Divyesh@1692 <divyeshvariya29@gmail.com>
2024-05-20 19:41:04 +0530

    Functions

---
+++ Functions/functions.js
@@ -0,0 +1,53 @@
+function sum()
+{
+    let a = parseInt(document.getElementById('n1').value);
+    let b = parseInt(document.getElementById('n2').value);
+    let c = parseInt(document.getElementById('n3').value);
+    let sum = a+b+c;
+    document.getElementById('res').innerHTML = "Sum Of Given Numbers Is : "+sum;
+}
+function sub()
+{
+    let a = parseInt(document.getElementById('n1').value);
+    let b = parseInt(document.getElementById('n2').value);
+    let c = parseInt(document.getElementById('n3').value);
+    let d = parseInt(document.getElementById('n4').value);
+
+    let sub = a-b-c-d;
+    document.getElementById('res').innerHTML = "Substraction Of Given Numbers Is : "+sub;
+}
+function multi()
+{
+    let a = parseInt(document.getElementById('n1').value);
+    let multi = a*50;
+    document.getElementById('res').innerHTML = "Result : "+multi;
+}
+
+function square()
+{
+    let a = parseInt(document.getElementById('n1').value);
+    let square = a*a;
+    document.getElementById('res').innerHTML = "Square : "+square;
+}
+function name_age()
+{
+    let name = document.getElementById('name').value
+    let age = parseInt(document.getElementById('age').value);
+    document.getElementById('res').innerHTML = "Name : "+name + "<br>Age : "+age;
+    
+}
+
+function odd_even()
+{
+    let a = parseInt(document.getElementById('n1').value);
+    let res;
+    if(a%2==0)
+        {
+            res = "Even";
+        }
+    else{
+        res = "Odd";
+    }
+    document.getElementById('res').innerHTML = "Given Nimber Is : "+res;
+
+}
