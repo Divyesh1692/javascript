f55c5cbd255d9a5394a80f4c50837bf670778a43
Divyesh1692 <divyeshvariya1692@gmail.com>
2024-05-30 00:41:15 +0530

    PR Array Methods

---
+++ PR Array Methods/1.js
@@ -0,0 +1,22 @@
+let list = [];
+function add()
+{
+    let item = document.getElementById('1').value;
+    let list = [];
+    list.push(item);
+    let t = document.getElementById('t');
+    let tr = document.createElement('tr');
+    let td = document.createElement('td');
+    td.innerHTML=item;
+    tr.append(td);
+    t.append(tr);
+
+}
+
+function remove()
+{
+    let item = document.getElementById('2').value;
+    console.log(item);
+    delete list[0];
+    console.log(list.indexOf("Apple"));
+}
