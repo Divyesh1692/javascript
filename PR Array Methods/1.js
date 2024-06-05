9a27af51927f9b96f31a6f4f4e3a3f245a124e6f
Divyesh1692 <divyeshvariya1692@gmail.com>
2024-06-05 21:08:24 +0530

    5/6/2024

--- PR Array Methods/1.js
+++ PR Array Methods/1.js
@@ -1,21 +1,32 @@
 let list = [];
-function add()
-{
-    let item = document.getElementById('1').value;
-    let list = [];
-    list.push(item);
-    let t = document.getElementById('t');
-    let tr = document.createElement('tr');
-    let td = document.createElement('td');
-    td.innerHTML=item;
-    tr.append(td);
-    t.append(tr);
+function add() {
+  let item = document.getElementById("1").value;
 
+  list.push(item);
+  console.log(list);
+  let t = document.getElementById("t");
+  let tr = document.createElement("tr");
+  let td = document.createElement("td");
+  td.innerHTML = item;
+  td.id = "td" + item;
+  tr.append(td);
+  tr.id = item;
+  t.append(tr);
 }
 
-function remove()
-{
-    let item = document.getElementById('2').value;
-    console.log(item);
-    delete list[0];
+function rem() {
+  let item = document.getElementById("rem").value;
+  let row = document.getElementById(item);
+  row.remove();
+}
+
+function Change() {
+  let newVal = document.getElementById("n").value;
+  let oldVal = document.getElementById("o").value;
+
+  let row = document.getElementById(oldVal);
+  row.id = newVal;
+  let td = document.getElementById("td" + oldVal);
+  td.innerHTML = newVal;
+  td.id = "td" + newVal;
 }
--- Table/emp.js
+++ Table/emp.js
@@ -1,4 +1,4 @@
-// const data = 
+// const data =
 // {
 //     id : document.getElementById('i').value,
 //     name : document.getElementById('n').value,
@@ -6,23 +6,40 @@
 //     salary : document.getElementById('s').value,
 // };
 
-function d()
-{
-    let id = document.getElementById('i').value;
-    let name = document.getElementById('n').value;
-    let designation = document.getElementById('des').value;
-    let salary = document.getElementById('s').value;
-    let t = document.getElementById('op');
-    let td_id = document.createElement('td');
-    let td_name = document.createElement('td');
-    let td_des = document.createElement('td');
-    let td_sal = document.createElement('td');
-    let tr = document.createElement('tr');
-    td_id.innerHTML = id;
-    td_name.innerHTML= name;
-    td_des.innerHTML= designation;
-    td_sal.innerHTML = salary;
-    tr.append(td_id,td_name,td_des,td_sal);
-    t.append(tr);
+// Creating object
+let emp = {
+  id: 101,
+  name: "Demo",
+  designation: "Demo",
+  salary: 100,
+};
+
+function d() {
+  // Getting values
+  emp.id = document.getElementById("i").value;
+  obj.name = document.getElementById("n").value;
+  obj.designation = document.getElementById("des").value;
+  obj.salary = document.getElementById("s").value;
+
+  // Table object
+  let t = document.getElementById("op");
+
+  // Creating elements
+  let td_id = document.createElement("td");
+  let td_name = document.createElement("td");
+  let td_des = document.createElement("td");
+  let td_sal = document.createElement("td");
+  let tr = document.createElement("tr");
+
+  // Assign data
+  td_id.innerHTML = id;
+  td_name.innerHTML = name;
+  td_des.innerHTML = designation;
+  td_sal.innerHTML = salary;
+
+  // Adding in row
+  tr.append(td_id, td_name, td_des, td_sal);
+
+  // Adding in table
+  t.append(tr);
 }
- 
